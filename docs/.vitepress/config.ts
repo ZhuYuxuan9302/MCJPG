d3edb59edf749c5c93c882cc92ea845974e5df6f
Zhu_Yuxuan <suifeng9302@gmail.com>
2024-08-22 15:23:34 +0800

    尝试更新主页

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -26,12 +26,25 @@ export default defineConfig({
   /* 主题配置 */
   themeConfig: {
     i18nRouting: false,
-
+    siteTitle: 'MCJPG组织', //站点标题
+    description: '一个Minecraft服务器宣传组织',//MCJPG
     logo: '/logo.png',
 
-    nav,
-    sidebar,
-
+    nav: [
+        { text: "成员服", link: "/" },
+        { text: "规则", link: "/rules/" },
+        { text: "组织架构", link: "/structure/" },
+        { text: "状态监测", link: "https://status.mcjpg.org/" },
+        {
+          text: "友情链接",
+          items: [
+            { text: "随风的个人网站", link: "https://zhuyuxuan.link/" },
+            { text: "金苹果论坛", link: "https://mcodw.com/" },
+            { text: "笨蛋开服指南", link: "https://yizhan.wiki/NitWikit/" },
+            { text: "风梨影视", link: "https://v.flweb.cn/" }
+          ]
+        }
+  ],
     /* 右侧大纲配置 */
     outline: {
       level: 'deep',
