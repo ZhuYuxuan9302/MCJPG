71e8939582f3d01e2d17003c2d7c31c0f2fb31bd
Zhu_Yuxuan <suifeng9302@gmail.com>
2024-08-22 15:27:18 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 
   lang: 'zh-CN',
-  title: 'MCJPG组织网站',
-  description: 'Minecraft服务器集体宣传组织 (MSJPG)-一个致力于Minecraft技术交流和服务器宣传的新兴组织',
+  title: '茂茂物语',
+  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
   head,
 
   lastUpdated: true,
@@ -26,36 +26,23 @@ export default defineConfig({
   /* 主题配置 */
   themeConfig: {
     i18nRouting: false,
-    siteTitle: 'MCJPG组织', //站点标题
-    description: '一个Minecraft服务器宣传组织',//MCJPG
+
     logo: '/logo.png',
 
-    nav: [
-        { text: "成员服", link: "/" },
-        { text: "规则", link: "/rules/" },
-        { text: "组织架构", link: "/structure/" },
-        { text: "状态监测", link: "https://status.mcjpg.org/" },
-        {
-          text: "友情链接",
-          items: [
-            { text: "随风的个人网站", link: "https://zhuyuxuan.link/" },
-            { text: "金苹果论坛", link: "https://mcodw.com/" },
-            { text: "笨蛋开服指南", link: "https://yizhan.wiki/NitWikit/" },
-            { text: "风梨影视", link: "https://v.flweb.cn/" }
-          ]
-        }
-  ],
+    nav,
+    sidebar,
+
     /* 右侧大纲配置 */
     outline: {
       level: 'deep',
       label: '目录',
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/MineJPGcraft/MCJPG' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
 
     footer: {
-      message: 'MCJPG服务器宣传组织',
-      copyright: 'Copyright © 2024-present <a href="https://mcjpg.org/">MCJPG</a>— —<a href="https://github.com/MineJPGcraft/MCJPG">我们的仓库</a>',
+      message: '如有转载或 CV 的请标注本站原文地址',
+      copyright: 'Copyright © 2019-present maomao',
     },
 
     lastUpdated: {
