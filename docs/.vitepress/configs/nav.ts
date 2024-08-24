6ae0c9f1612227fed0410df1f3355ae9de4b30ea
Zhu_Yuxuan <suifeng9302@gmail.com>
2024-08-24 12:04:27 +0800

    更新导航栏

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -7,12 +7,12 @@ export const nav: DefaultTheme.Config['nav'] = [
     text: '组织架构',
     link: '/structure/',
   },
+  { text: '社区论坛', link:'https://bbs.mcjpg.org/'},
   { text: '状态监测', link: 'https://status.mcjpg.org/' },
   {
     text: "友情链接",
               items: [
                 { text: "随风的个人网站", link: "https://zhuyuxuan.link/" },
-                { text: "金苹果论坛", link: "https://mcodw.com/" },
                 { text: "笨蛋开服指南", link: "https://yizhan.wiki/NitWikit/" },
                 { text: "风梨影视", link: "https://v.flweb.cn/" }
               ]
