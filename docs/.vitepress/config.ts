731d4a55be9667f83eb12e31a608a3c4b78bf2c1
Zhu_Yuxuan <suifeng9302@gmail.com>
2024-08-25 18:31:11 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,8 +4,6 @@ import MarkdownPreview from 'vite-plugin-markdown-preview'
 
 import { head, nav, sidebar } from './configs'
 
-const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
-
 export default defineConfig({
   outDir: '../dist',
 
