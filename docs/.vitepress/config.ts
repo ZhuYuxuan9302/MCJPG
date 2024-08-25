4cbe334b346b8db3881a0ff9212c56f838a21308
Zhu_Yuxuan <suifeng9302@gmail.com>
2024-08-25 18:17:19 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,6 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
-  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 
   lang: 'zh-CN',
   title: 'MCJPG组织网站',
