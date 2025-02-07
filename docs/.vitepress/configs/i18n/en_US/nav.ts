b84e1e4afccb777f5c763e90ee9ad85d6c8ff99e
cygbs <ygbs@bugcraft.org>
2025-02-07 17:54:55 +0800

    add bugcraft link
    nothing

--- docs/.vitepress/configs/i18n/en_US/nav.ts
+++ docs/.vitepress/configs/i18n/en_US/nav.ts
@@ -23,7 +23,7 @@ export const nav: DefaultTheme.Config['nav'] = [
               items: [
                 { text: "随风的个人网站", link: "https://zhuyuxuan.link/" },
                 { text: "MSCPO", link: "https://mscpo.netlify.app/" },
-                { text: "风梨社区", link: "https://www.flweb.cn/" },
+                { text: "风梨社区", link: "https://bugcraft.org/" },
               ]
   }
 ]
